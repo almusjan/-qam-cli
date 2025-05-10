1bca61328d01ef9051b58f98c5fb77a25ddb64a7
almusjan <almusjan.q@gmail.com>
2025-05-10 19:58:17 +0300

    remove old src files

--- src/commands/create-tailwind/create-tailwind-with-angular.command.js
+++
@@ -1,6 +0,0 @@
-import {cloner} from "../../git-cloner.js";
-export const createTailwindWithAngularCommand = (name, template) => {
-    const url = 'https://github.com/almusjan/angular-tailwind-starter-template';
-    const message = `\nTemplate created! To get started run: \n  cd ${name}\n  npm install\n  ng serve`;
-    cloner(url, name, template, message);
-}
